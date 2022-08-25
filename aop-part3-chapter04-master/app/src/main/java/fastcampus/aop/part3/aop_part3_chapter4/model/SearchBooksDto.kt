349f8d00afa6b97442eb6ad3df90d2a71a728f2b
현영우 <ieeh0122@naver.com>
2022-08-25 17:50:55 +0900

    삭제

--- aop-part3-chapter04-master/app/src/main/java/fastcampus/aop/part3/aop_part3_chapter4/model/SearchBooksDto.kt
+++
@@ -1,7 +0,0 @@
-package fastcampus.aop.part3.aop_part3_chapter4.model
-
-import com.google.gson.annotations.SerializedName
-
-data class SearchBooksDto(
-    @SerializedName("items") val books: List<Book>
-)
